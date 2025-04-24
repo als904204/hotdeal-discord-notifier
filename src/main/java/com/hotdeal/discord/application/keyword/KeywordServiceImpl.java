84b25a62435fd3ddbf14ce61aa599bdbef28f00a
Min <als904204@naver.com>
2025-04-24 16:45:55 +0900

    docs: Keyword 비즈니스 로직 코드가이드라인 작성

--- src/main/java/com/hotdeal/discord/application/keyword/KeywordServiceImpl.java
+++ src/main/java/com/hotdeal/discord/application/keyword/KeywordServiceImpl.java
@@ -9,8 +9,30 @@ import org.springframework.stereotype.Service;
 public class KeywordServiceImpl implements KeywordService{
 
 
+    /**
+     * RegisterCommandHandler 클래스에서 넘어온 값을 이용합니다.
+     * 사용자가 요청한 키워드를 DB에 저장하는 비즈니스 로직 작성
+     * @param keyword 사용자가 등록한 키워드
+     * @param userId 사용자 고유 Discord ID
+     */
     @Override
     public void registerKeyword(String keyword, String userId) {
+        /**
+         * DB 저장하기 전, 해당 사용자가 이미 저장한 키워드인지 검증
+         * - 중복 저장이라면 커스텀 예외 발생
+         */
+
+        /**
+         * Keyword 엔티티 생성
+         */
+
+        /**
+         * 생성된 Keyword 엔티티 DB저장
+         */
+
+        /**
+         * 저장된 Keyword 로깅
+         */
         log.info("키워드 등록 keyword: {}, userId: {}", keyword, userId);
     }
 

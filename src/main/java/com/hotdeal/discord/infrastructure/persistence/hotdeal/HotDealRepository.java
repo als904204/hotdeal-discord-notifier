0d19bb14f414bf4e5301c93175e1936c9d08b933
Min <als904204@naver.com>
2025-04-28 13:47:45 +0900

    feat: 핫딜 상태 업데이트 도메인 로직

--- src/main/java/com/hotdeal/discord/infrastructure/persistence/hotdeal/HotDealRepository.java
+++ src/main/java/com/hotdeal/discord/infrastructure/persistence/hotdeal/HotDealRepository.java
@@ -1,10 +1,14 @@
 package com.hotdeal.discord.infrastructure.persistence.hotdeal;
 
+import com.hotdeal.discord.domain.hotdeal.CommunityType;
 import com.hotdeal.discord.domain.hotdeal.HotDeal;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HotDealRepository extends JpaRepository<HotDeal, Long> {
+
+    Optional<HotDeal> findByCommunityCodeAndPostId(CommunityType communityCode, String postId);
 
 }

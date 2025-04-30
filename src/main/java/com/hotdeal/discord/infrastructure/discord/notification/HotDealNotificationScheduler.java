aeb4ad958bf3a8e43b83ab2d2a274e4db4e377f6
Min <als904204@naver.com>
2025-04-30 13:31:41 +0900

    feat: 핫딜 알람 스케쥴러 구현

---
+++ src/main/java/com/hotdeal/discord/infrastructure/discord/notification/HotDealNotificationScheduler.java
@@ -0,0 +1,23 @@
+package com.hotdeal.discord.infrastructure.discord.notification;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class HotDealNotificationScheduler {
+
+    private final HotDealNotificationSender notificationSender;
+
+    // 5분마다 실행
+    @Scheduled(fixedRate = 200000)
+    public void scheduleHotDealNotifications() {
+        log.info("핫딜 알림 스케줄러 실행 시작");
+        notificationSender.processKeywordMatchNotifications();
+        log.info("핫딜 알림 스케줄러 실행 완료");
+    }
+
+}

9078e2eba68254d276d5fbbf5ab9a0a97a64a34d
Min <als904204@naver.com>
2025-05-08 15:02:57 +0900

    feat: 핫딜 만료 로직 구현

--- src/main/java/com/hotdeal/discord/application/hotdeal/HotDealServiceImpl.java
+++ src/main/java/com/hotdeal/discord/application/hotdeal/HotDealServiceImpl.java
@@ -3,15 +3,20 @@ package com.hotdeal.discord.application.hotdeal;
 import com.hotdeal.discord.domain.hotdeal.HotDeal;
 import com.hotdeal.discord.domain.hotdeal.HotDealStatus;
 import com.hotdeal.discord.infrastructure.persistence.hotdeal.HotDealRepository;
+import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class HotDealServiceImpl implements HotDealService{
 
     private final HotDealRepository hotDealRepository;
+    private static final long EXPIRATION_HOURS = 6; // 만료 시간 (6시간)
 
     @Override
     public List<HotDeal> findActiveHotDeals() {
@@ -46,6 +51,21 @@ public class HotDealServiceImpl implements HotDealService{
         }
 
         return messageBuilder.toString();
+    }
+
+    @Transactional
+    @Override
+    public void expireOldHotDeals() {
+        List<HotDeal> activeDeals = hotDealRepository.findByStatus(HotDealStatus.ACTIVE);
+        LocalDateTime sixHoursAgo = LocalDateTime.now().minusHours(EXPIRATION_HOURS);
+        int cnt = 0;
+        for (HotDeal deal : activeDeals) {
+            if (deal.getCreatedAt().isBefore(sixHoursAgo)) {
+                deal.updateStatusIfEnded(HotDealStatus.END);
+                ++cnt;
+            }
+        }
+        log.info("{}건, 핫딜 상태 변경 완료", cnt);
     }
 
 }
